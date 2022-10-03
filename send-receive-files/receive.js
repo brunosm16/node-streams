dfba0a755fc742de65addfec9bc30ba0dcb680ca
brunosm16 <brunoskr23@gmail.com>
2022-10-03 17:44:45 -0300

    implement send-receive files using stream.

--- send-receive-files/receive.js
+++ send-receive-files/receive.js
@@ -1,22 +1,29 @@
-import { createWriteStream } from "fs";
+import { createReadStream, createWriteStream } from "fs";
 import { createServer } from "http";
-import { join, basename } from "path";
+import { basename, join } from "path";
 import { createGunzip } from "zlib";
 import { PORT } from "./constants.js";
+import getDirName from "../utils/get-dir-name.js";
+
+const __dirname = getDirName(import.meta.url);
+
+const onFinish = (res) => {
+  res.writeHead(201, { "Content-Type": "text/plain" });
+  res.end("Receive server finished");
+  console.log("Files received");
+};
 
 const server = createServer((req, res) => {
-  const file = basename(req.headers["x-filename"]);
+  const file = req.headers["x-filename"];
 
-  const dest = join("files-received", file);
+  const filename = basename(file);
+
+  const pathToSave = join(__dirname, "files-received", filename);
 
   req
     .pipe(createGunzip())
-    .pipe(createWriteStream(dest))
-    .on("finish", () => {
-      res.writeHead(201, { "Content-Type": "text-plain" });
-      res.end("Server closed\n");
-      console.log(`Files successfully sent`);
-    });
+    .pipe(createWriteStream(pathToSave))
+    .on("finish", () => onFinish(res));
 });
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
--- send-receive-files/send.js
+++ send-receive-files/send.js
@@ -1,23 +1,22 @@
-import { createGzip } from "zlib";
-import { createReadStream } from "fs";
 import { request } from "http";
-import createHttpHeader from "../utils/createHttpHeader.js";
-import getDirName from "../utils/getDirName.js";
-import { PORT } from "./constants.js";
-import createHttpOptions from "../utils/createHttpOptions.js";
+import { PORT, HOSTNAME } from "./constants.js";
+import createHttpOptions from "../utils/create-http-options.js";
+import createHttpHeaders from "../utils/create-http-headers.js";
+import getDirName from "../utils/get-dir-name.js";
+import { createReadStream } from "fs";
+import { createGzip } from "zlib";
 
 const __dirname = getDirName(import.meta.url);
-
 const fileDir = `${__dirname}/files-to-send/file-1.txt`;
 
-const headers = createHttpHeader(fileDir, "gzip");
-const putHttpOptions = createHttpOptions(headers, "localhost", PORT);
+const headers = createHttpHeaders("octet-stream", "gzip", fileDir);
+const reqOptions = createHttpOptions(HOSTNAME, PORT, "PUT", "/", headers);
 
-const req = request(putHttpOptions, (res) =>
-  console.log(`Server response status : ${res.statusCode}`)
+const req = request(reqOptions, (res) =>
+  console.log(`Response : ${res.statusCode}`)
 );
 
 createReadStream(fileDir)
   .pipe(createGzip())
   .pipe(req)
-  .on("finish", () => console.log("File was sent"));
+  .on("finish", () => console.log("File sent"));
